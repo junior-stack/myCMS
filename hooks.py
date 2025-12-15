8c41cf54bde989f1502d2a03e4e89fedb0fea150
yezhou2 <jaxyz.ye@mail.utoronto.ca>
2025-12-14 20:35:30 -0500

    add support of callout and tab with file titles

--- hooks.py
+++ hooks.py
@@ -8,13 +8,13 @@ x_intent = "https://x.com/intent/tweet"
 fb_sharer = "https://www.facebook.com/sharer/sharer.php"
 
 # Pattern to match ```lang group:groupName ... ```
-GROUP_PATTERN = re.compile(
-    r"```(\w+)\s+group:([^\n]+)\n(.*?)```",
+CODE_BLOCK_PATTERN = re.compile(
+    r"```(\w+)\s+([^\n]+)\n(.*?)```",
     re.DOTALL
 )
 
-# FOLD_PATTERN = re.compile(r"(```\w+)(?:\s+group:[^\n]+)?\s+fold")
-FOLD_PATTERN = re.compile(r"(```\w+)(\s+group:[^\n]+)?\s+fold")
+CALLOUT_PATTERN = re.compile(r"> \[!(\w+)\]([+|-])?([^\n]+)?\n>(.*?)\n[^>]", re.DOTALL)
+
 
 HEADER_PATTERN = re.compile(r'^(#+)', re.MULTILINE)
 
@@ -26,14 +26,6 @@ TABS_PATTERN = re.compile(r"~~~tabs\s*(.*?)~~~", re.DOTALL)
 
 TAB_PATTERN = r"---\s*tab\s*"
 
-def remove_fold_flags(content: str) -> str:
-    """
-    Removes ' fold' from code block opening lines.
-    Works with:
-    ```python fold
-    ```python group:foo fold
-    """
-    return FOLD_PATTERN.sub(r"\1\2", content)
 
 def transform_group_tabs(content: str) -> str:
     """
@@ -47,14 +39,27 @@ def transform_group_tabs(content: str) -> str:
     ...
     ```
     """
+    FILE_PATTERN =r"file:([^\s]+)"
     def repl(match):
         lang = match.group(1).strip()
-        # group = match.group(2).strip()
+        flags = match.group(2).strip()
         code = match.group(3)
-        code = "\n".join("\t" + line for line in code.splitlines())
-        return f'=== "{lang}"\n\t```{lang}\n{code}\n\t```\n'
+        tab_title = lang
+        if "group:" in flags:
+            code = "\n".join("\t" + line for line in code.splitlines())
+            tab_title = lang
+            if "file:" in flags:
+                tab_title = re.search(FILE_PATTERN, flags).group(1)
+            return f'=== "{tab_title}"\n\t```{lang}\n{code}\n\t```\n'
+        elif "file:" in flags:
+            title = re.search(FILE_PATTERN, flags).group(1)
+            return f'```{lang} title="{title}"\n{code}\n\t```\n'
+        elif "fold" in flags:
+            return f'```{lang}\n{code}\n```\n'
+        else:
+            return match.group(0)  # no change
 
-    return GROUP_PATTERN.sub(repl, content)
+    return CODE_BLOCK_PATTERN.sub(repl, content)
 
 
 def increase_header_level(content: str) -> str:
@@ -96,10 +101,26 @@ def parse_picture(content: str) -> str:
         return f"![](../images/{pic})"
     return PICTURE_PATTERN.sub(repl, content)
 
+def parse_callouts(content: str) -> str:
+    def repl(match):
+        callout_type = match.group(1).lower()
+        isCollapsed = match.group(2)
+        title = match.group(3)
+        calloutcontent = match.group(4).strip()
+        
+        calloutheader = f"???+ {callout_type} "
+        if isCollapsed == "-":
+            calloutheader = f"??? {callout_type} "
+        if title:
+            calloutheader += f'"{title.strip()}"'
+        return calloutheader + "\n\t" + calloutcontent.replace("\n>", "\n\t") + "\n"
+        
+    return CALLOUT_PATTERN.sub(repl, content)
+
 def transform(content: str) -> str:
     content = transform_Bullet_start(content)
+    content = parse_callouts(content)
     content = convert_tabs_block(content)
-    content = remove_fold_flags(content)
     content = transform_group_tabs(content)
     content = increase_header_level(content)
     content = parse_picture(content)
@@ -122,10 +143,11 @@ def on_page_markdown(markdown, page, config, files):
     """)
 
 # if __name__ == "__main__":
-#     file_path ="./docs/CheatSheet.md"
+#     file_path ="./docs/posts/exp.md"
 #     with open(file_path, "r", encoding="utf-8") as f:
 #         file_content = f.read()
-#         my_string = convert_tabs_block(file_content)
+#         my_string = parse_callouts(file_content)
+#         print(my_string)
 #         with open("my_file.md", "w", encoding="utf-8") as file_object:
 #             # Write the string to the file
 #             file_object.write(my_string)
